d053e3fc557f764d2f27a074c617a669e8b7fb49
Sam O'Shaughnessy <samoshaughnessy123@hotmail.com>
2022-11-09 11:21:05 +0800

    local storage

--- src/components/GetData.js
+++ src/components/GetData.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./GetData.css";
 
 export default class GetData extends React.Component {
   constructor(props) {
@@ -8,6 +9,16 @@ export default class GetData extends React.Component {
       pokemon: [],
       name: "",
     };
+  }
+
+  testMe() {
+    console.log("This class, layer 1", this);
+
+    const anotherTest = () => {
+      console.log(this);
+    };
+
+    anotherTest();
   }
 
   callAPI = (name) => {
@@ -21,9 +32,31 @@ export default class GetData extends React.Component {
           weight: response.weight,
         };
 
-        this.setState({
-          pokemon: [...this.state.pokemon, pokemon],
-        });
+        console.log(this.state.pokemon);
+
+        if (this.state.pokemon && this.state.pokemon.length > 0) {
+          this.setState(
+            {
+              pokemon: [...this.state.pokemon, pokemon],
+            },
+            () => {
+              let itemToStore = JSON.stringify(this.state.pokemon);
+              // localStorage only stores strings.~!>!!>!>>!
+              localStorage.setItem("pokemon", itemToStore);
+            }
+          );
+        } else {
+          this.setState(
+            {
+              pokemon: [pokemon],
+            },
+            () => {
+              let itemToStore = JSON.stringify(this.state.pokemon);
+              // localStorage only stores strings.~!>!!>!>>!
+              localStorage.setItem("pokemon", itemToStore);
+            }
+          );
+        }
       });
   };
 
@@ -34,16 +67,36 @@ export default class GetData extends React.Component {
   };
 
   componentDidMount() {
-    this.callAPI("mew");
+    // only if we dont have local storage!
+    // this.callAPI("mew");
+
+    // getting currently stored pokemon from localStorage
+    let currenlyStoredPokemon = localStorage.getItem("pokemon");
+    console.log(currenlyStoredPokemon);
+    let parsedCurrentlyStoredPokemon = JSON.parse(currenlyStoredPokemon);
+    console.log(parsedCurrentlyStoredPokemon);
+    if (parsedCurrentlyStoredPokemon === null) {
+      parsedCurrentlyStoredPokemon = [];
+    }
+    this.setState({
+      pokemon: parsedCurrentlyStoredPokemon,
+    });
   }
 
   componentDidUpdate() {
     console.log(this.state.pokemon);
   }
 
+  clearStorage = () => {
+    localStorage.clear();
+    this.setState({
+      pokemon: [],
+    });
+  };
+
   render() {
     return (
-      <div>
+      <div className="container">
         <h1>API</h1>
         <input
           value={this.state.name}
@@ -53,19 +106,26 @@ export default class GetData extends React.Component {
         <button onClick={() => this.callAPI(this.state.name)}>
           Click Me for Pokemon
         </button>
+        <div className="pokemonContainer">
+          {this.state.pokemon && this.state.pokemon.length > 0 ? (
+            this.state.pokemon.map((monster) => (
+              <div className="card">
+                <img src={monster.image} alt={monster.name} />
+                <p>
+                  {monster.name} - {monster.weight}
+                </p>
+              </div>
+            ))
+          ) : (
+            <p>No Pokemon yet</p>
+          )}
+        </div>
 
-        {this.state.pokemon.length > 0 ? (
-          this.state.pokemon.map((monster) => (
-            <div>
-              <img src={monster.image} alt={monster.name} />
-              <p>
-                {monster.name} - {monster.weight}
-              </p>
-            </div>
-          ))
-        ) : (
-          <p>No Pokemon yet</p>
-        )}
+        <button onClick={() => this.clearStorage()}>
+          Click to delete stored pokemon
+        </button>
+
+        <button onClick={() => this.testMe()}>Click to test</button>
       </div>
     );
   }
